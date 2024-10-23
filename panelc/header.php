df4b8ce9b94ac0ac20173bc3fc720dafbb9df147
Orel Vilchise <orel.vilchis@morsa.com.mx>
2024-10-22 20:09:16 -0500

    ajuste para usuario de Zuri

--- panelc/header.php
+++ panelc/header.php
@@ -207,7 +207,7 @@
           <li class="nav-item">
             <div class="d-flex sidebar-profile">
               <div class="sidebar-profile-image">
-                <img src="images/faces/face29.png" alt="image">
+                <img src="images/faces/face30.png" alt="image">
                 <span class="sidebar-status-indicator"></span>
               </div>
               <div class="sidebar-profile-name">
@@ -235,6 +235,12 @@
             <a class="nav-link" href="calendario.php">
               <img src="images/iconos/calendar.png" style="width: 25px; margin-right: 14px; margin-left: -3px;" alt="">
               <span class="menu-title">Calendario</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="semanas_esp.php">
+              <img src="images/iconos/calendar.png" style="width: 25px; margin-right: 14px; margin-left: -3px;" alt="">
+              <span class="menu-title">Semanas Especiales</span>
             </a>
           </li>
           <li class="nav-item">
